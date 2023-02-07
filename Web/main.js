bed1a9c65fc70c0b84c6e7cd44126757ac369381
syuuma <itosyuuma@gmail.com>
2023-02-07 22:23:45 +0900

    デザインを変更

--- Web/main.js
+++ Web/main.js
@@ -7,32 +7,27 @@ async function loading_effect(num_step) {
     for (let i = 0; i <= num_step; i++) {
         $(".bar").css("width", (i / num_step) * 100 + "%");
         await _sleep(314);
+        console.log((i / num_step) * 100 + "%");
     }
+    $(".loading").hide();
+}
+
+function outputSelectedValueAndText(obj) {
+    let idx = obj.selectedIndex;
+    let value = obj.options[idx].value; // 値
+    $(".model-info").hide();
+    $("#" + value).show();
 }
 
 $(document).ready(function () {
     $("#send").on("click", async function () {
+        $("#send-div").hide();
         let prompt = $("#input_prompt").val();
-        let num_step = $("num-step").val();
-        console.log(prompt);
-        loading_effect(num_step);
-        fetch("https://2ctn99fmhn-496ff2e9c6d22116-8000-colab.googleusercontent.com/test?prompt=%22%22&negative_prompt=%22%22&seed=%220%22", {
-            method: "GET",
-            mode: "cors",
-        })
-            .then((response) => {
-                if (response.ok) {
-                    return response.json();
-                }
-                // 404 や 500 ステータスならここに到達する
-                throw new Error("Network response was not ok.");
-            })
-            .then((resJson) => {
-                console.log(JSON.stringify(resJson));
-            })
-            .catch((error) => {
-                // ネットワークエラーの場合はここに到達する
-                console.error(error);
-            });
+        let negative_prompt = $("#negative_prompt").val();
+        let model = $("#model").val();
+        let seed = $("#seed").val();
+        let num_step = $("#num-step").val();
+        await loading_effect(num_step);
+        $("#send-div").show();
     });
 });
